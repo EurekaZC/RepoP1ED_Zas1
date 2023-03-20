f145491327dfecfcd7c0df36beac3906cd2549dd
Aloy <Aloy@DESKTOP-LFCO79F>
2023-03-20 17:43:52 +0100

    Primera versión programa control versiones.

---
+++ Proyecto_1/src/paquete1/Principal.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Cristina Zas Perez
+		System.out.println("Programa que nos permitirá hacer un control de versiones con Git (o eso me comentan).");
+
+	}
+
+}
