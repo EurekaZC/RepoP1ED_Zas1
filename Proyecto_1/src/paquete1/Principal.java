3597555f9ab79745710aa787b2507c1d702deced
Aloy <Aloy@DESKTOP-LFCO79F>
2023-03-20 17:48:47 +0100

    Segunda modificacion

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// Cristina Zas Perez
-		System.out.println("Programa que nos permitirá hacer un control de versiones con Git (o eso me comentan).");
+		System.out.println("Programa que nos permitirá hacer un control de versiones con Git (o eso me comentan)."
+				+ "Segunda modificacion");
 
 	}
 
